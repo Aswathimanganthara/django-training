3b5a1f0704a98d03ce8ca0a2fd2e53d0c67278a4
Aswathimanganthara <44120202+Aswathimanganthara@users.noreply.github.com>
2020-08-15 23:36:01 +0530

    task1

--- training/settings.py
+++ training/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Aswathi',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'training.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
